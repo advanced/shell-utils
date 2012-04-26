26afa917f23242080ab62aad249314fccd048d3d
advanced <rian@advanced.net.tw>
2012-04-27 04:09:46 +0800

    added node git and nginx

--- bootstrap-ruby.sh
+++ bootstrap-ruby.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 sudo apt-get -y update
-sudo apt-get -y install build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev
+sudo apt-get -y install build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev git nginx
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get -y install nodejs 
 cd /tmp
 sudo wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p125.tar.gz
 sudo tar -xvzf ruby-1.9.3-p125.tar.gz
