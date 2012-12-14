a9f65920ce46db63ca2b559491371fbd338838b7
advanced <rian@advanced.net.tw>
2012-12-14 18:22:29 +0800

    Update bootstrap-ruby.sh
    updated to new ruby and dep

--- bootstrap-ruby.sh
+++ bootstrap-ruby.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 sudo apt-get -y update
-sudo apt-get -y install build-essential libsqlite3-dev zlib1g-dev libssl-dev libreadline-gplv2-dev libxslt-dev libxml2-dev
-libyaml-dev git nginx
-sudo add-apt-repository ppa:chris-lea/node.js
-sudo apt-get update
-sudo apt-get -y install nodejs 
+sudo apt-get -y install build-essential openssl libreadline6 libreadline6-dev 
+curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev
+libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkgconfig
 cd /tmp
-sudo wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p125.tar.gz
-sudo tar -xvzf ruby-1.9.3-p125.tar.gz
-cd ruby-1.9.3-p125/
+sudo wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p327.tar.gz
+sudo tar -xvzf ruby-1.9.3-p327.tar.gz
+cd ruby-1.9.3-p327/
 sudo ./configure --prefix=/usr/local
 sudo make
 sudo make install
+
+ 
+ 
+
